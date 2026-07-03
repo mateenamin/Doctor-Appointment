f3ef9d601c52550010447d42c8aadf04c87a256b
Mateen Amin <mateenamin1122@gmail.com>
2026-07-04 02:07:52 +0500

    Add Appointment model

---
+++ server/models/appointment.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+
+const appointmentScheme = mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    doctorID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+
+    },
+    time: {
+        type: String,
+        required: true,
+
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'cancelled'],
+        default: 'pending',
+
+    }
+},
+    { timestamps: true }
+)
+
+const Appointment = mongoose.model('Appointment', appointmentScheme)
+
+
+export default Appointment
