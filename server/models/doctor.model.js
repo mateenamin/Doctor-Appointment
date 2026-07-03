6b4375efc3989219b0ef723cb8950366f0e17072
Mateen Amin <mateenamin1122@gmail.com>
2026-07-04 01:50:29 +0500

    Add Doctor model

---
+++ server/models/doctor.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const doctorScheme = new mongoose.Schema({
+
+    name :{
+        type :String,
+        required : true ,
+    },
+    email : {
+        type :String,
+        required : true ,
+        unique : true,
+    }, password :{
+        type :String,
+        required : true ,
+    },
+    specialization : {
+        type : String,
+        required : true ,
+    },
+    experience  :{
+         type : Number,
+        required : true ,
+    },
+    fee : {
+        type : Number,
+        required : true ,
+    },
+    available  :{
+        type : Boolean ,
+        required : true,
+    }
+} ,
+
+   {  timestamps : true}
+)
+
+const Doctor = mongoose.model('Doctor' , doctorScheme)
+
+export default Doctor
