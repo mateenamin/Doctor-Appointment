c4b753a10d5532e0637df0e3e6d77ed1ecd3af60
Mateen Amin <mateenamin1122@gmail.com>
2026-07-04 03:14:17 +0500

    Add loginUser controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 
 export const signUser = async (req, res) => {
@@ -39,4 +41,49 @@ export const signUser = async (req, res) => {
 
 
 
+}
+
+
+export const loginUser = async (req, res) => {
+
+    try{
+        const { email, password } = req.body
+
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found!"
+        })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        return res.status(401).json({
+            message: "Incorrect password!"
+        })
+    }
+
+    const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' }
+    )
+
+    res.status(200).json({
+            message: "Login successful!",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+    }catch (err) {
+        res.status(500).json({ 
+            message: err.message 
+        })
+    }
 }
