9fe3a7fc220a5f62cee8d81ed0bec25b5e1bd97c
Mateen Amin <mateenamin1122@gmail.com>
2026-07-20 23:46:40 +0500

    Add DoctorDetail page complete

---
+++ client/src/pages/user/login.page.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "../../components/user/auth/loginForm.component"
+
+const LoginPage = () => {
+  return (
+    <main className="min-h-screen bg-[#EFF6FF] flex items-center justify-center px-6 py-12">
+      <LoginForm />
+    </main>
+  )
+}
+
+export default LoginPage
