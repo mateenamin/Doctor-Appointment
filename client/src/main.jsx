972f8885f6a6ae2d9aa64b206c326e336cd7470a
Mateen Amin <mateenamin1122@gmail.com>
2026-08-08 14:55:01 +0500

    Add AuthContext - frontend auth state management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,16 @@ import { createRoot } from 'react-dom/client'
 import Router from "./routes/index.routes"; //your router component
 import "./index.css";
 
+import { AuthProvider } from './context/AuthContext'
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
+
    <Router/>
+
+       </AuthProvider>
+
   </StrictMode>,
 )
